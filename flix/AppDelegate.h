37a7c3a163ebe928ba6874022e6e73bd42695392
Zelalem Tenaw Terefe <zterefe@fb.com>
2018-06-28 08:56:47 -0700

    Initial Commit

---
+++ flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flix
+//
+//  Created by Zelalem Tenaw Terefe on 6/28/18.
+//  Copyright © 2018 Zelalem Tenaw Terefe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  flix
+//
+//  Created by Zelalem Tenaw Terefe on 6/28/18.
+//  Copyright © 2018 Zelalem Tenaw Terefe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
